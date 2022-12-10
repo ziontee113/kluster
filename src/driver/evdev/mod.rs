0825405bfea05eb1bf43809db032a3dff516aa5d
ziontee113 <ziontee113@gmail.com>
2022-12-11 00:21:33 +0700

    ref(driver): use idiomatic functional iterrator

--- src/driver/evdev/mod.rs
+++ src/driver/evdev/mod.rs
@@ -1,6 +1,6 @@
 use crate::device::{Device, Keyboard};
 
-use super::Driver;
+use super::{Driver, Error};
 
 pub struct Evdev {}
 
@@ -11,17 +11,14 @@ impl Evdev {
 }
 
 impl Driver for Evdev {
-    fn get_devices(&self) -> Result<Vec<Box<dyn Device>>, super::Error> {
-        let devices = evdev::enumerate().map(|t| t.1).collect::<Vec<_>>();
-        let mut vec: Vec<Box<dyn Device>> = vec![];
-
-        for d in devices {
-            if let Some(path) = d.physical_path() {
-                let name = d.name().unwrap_or("N/a");
-                vec.push(Box::new(Keyboard::new(name, path)));
-            }
-        }
-
-        Ok(vec)
+    fn get_devices(&self) -> Result<Vec<Box<dyn Device>>, Error> {
+        Ok(evdev::enumerate()
+            .filter(|(_, d)| d.physical_path().is_some())
+            .map(|(_, d)| -> Box<dyn Device> {
+                let name = d.name().unwrap_or("N/A");
+                let path = d.physical_path().unwrap();
+                Box::new(Keyboard::new(name, path))
+            })
+            .collect())
     }
 }
