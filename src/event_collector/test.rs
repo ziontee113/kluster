b2d7bb0e749f792d3225a8508f7de565e2d0778f
ziontee113 <ziontee113@gmail.com>
2022-12-14 07:43:57 +0700

    ref(collector): further shorten tests using event!

--- src/event_collector/test.rs
+++ src/event_collector/test.rs
@@ -2,53 +2,42 @@ use crate::test_utilities::{i32_key_state_value_from_str, key_code_from_str, mip
 
 use super::*;
 
-macro_rules! ke {
-    ($a:ident $b:ident $c:expr => $d:expr) => {
-        KeyboardEvent::new(
+macro_rules! event {
+    ( $collector:ident receives: $a:ident $b:ident $c:ident $d:expr =>
+      $(sequence len $sequence:expr)*, $(pending_cluster len $pending_cluster:expr)* ) => {
+        let event = KeyboardEvent::new(
             Key::new(stringify!($a), key_code_from_str(stringify!($b)).unwrap()),
             i32_key_state_value_from_str(stringify!($c)),
             mipoch($d),
-        )
+        );
+        $collector.receive(&event);
+
+        $( assert!( $collector.sequence().len() == $sequence ); )*
+        $( assert!( $collector.pending_cluster().len() == $pending_cluster ); )*
     };
 }
 
 #[test]
-fn receive_event_within_interval() {
+fn key_down_event_within_interval() {
     let mut collector = Collector::new();
 
-    collector.receive(&ke!(L1 D Down => 0));
-    assert!(collector.pending_cluster().len() == 1);
-
-    collector.receive(&ke!(L1 F Down => 4));
-    assert!(collector.pending_cluster().len() == 2);
+    event!(collector receives: L1 D Down 0 => sequence len 0, pending_cluster len 1);
+    event!(collector receives: L1 F Down 4 => sequence len 0, pending_cluster len 2);
 }
 
 #[test]
-fn receive_event_outside_interval() {
+fn key_down_event_outside_interval() {
     let mut collector = Collector::new();
 
-    collector.receive(&ke!(L1 F Down => 0));
-    assert!(collector.pending_cluster().len() == 1);
-    assert!(collector.sequence().is_empty());
-
-    collector.receive(&ke!(R1 J Down => 22));
-    assert!(collector.pending_cluster().is_empty());
-    assert!(collector.sequence().len() == 2);
+    event!(collector receives: L1 D Down 0  => sequence len 0, pending_cluster len 1);
+    event!(collector receives: R1 J Down 22 => sequence len 2, pending_cluster len 0);
 }
 
 #[test]
-fn union_plus_key() {
+fn union_down_then_key_down_later() {
     let mut collector = Collector::new();
 
-    collector.receive(&ke!(L1 D Down => 0));
-    assert!(collector.pending_cluster().len() == 1);
-    assert!(collector.sequence().is_empty());
-
-    collector.receive(&ke!(L1 F Down => 10));
-    assert!(collector.pending_cluster().len() == 2);
-    assert!(collector.sequence().is_empty());
-
-    collector.receive(&ke!(R1 J Down => 100));
-    assert!(collector.pending_cluster().is_empty());
-    assert!(collector.sequence().len() == 2);
+    event!(collector receives: L1 D Down 0   => sequence len 0, pending_cluster len 1);
+    event!(collector receives: L1 F Down 10  => sequence len 0, pending_cluster len 2);
+    event!(collector receives: R1 J Down 100 => sequence len 2, pending_cluster len 0);
 }
