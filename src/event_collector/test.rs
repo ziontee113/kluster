3df6f66b65e1bf3a57abaed47708f2e91286bb36
ziontee113 <ziontee113@gmail.com>
2022-12-15 00:25:59 +0700

    feat(test): add event! and check! macros
    I'm still experimenting to find the best way to express intention and
    reduce boilerplate using Rust macros.
    
    These new event! and check! macros reads clear, but feels too verbose.
    The old event! macro (now ev!), is significantly shorter, but isn't
    "expressive" enough.

--- src/event_collector/test.rs
+++ src/event_collector/test.rs
@@ -2,7 +2,7 @@ use crate::test_utilities::{i32_key_state_value_from_str, key_code_from_str, mip
 
 use super::*;
 
-macro_rules! event {
+macro_rules! ev{
     ( $collector:ident receives: $a:ident $b:ident $c:ident $d:expr =>
       $(sequence $sequence:expr)*, $(pending_cluster $pending_cluster:expr)* ) => {
         let event = KeyboardEvent::new(
@@ -17,27 +17,57 @@ macro_rules! event {
     };
 }
 
+macro_rules! event {
+    ( $variable:ident receives: $device:ident $key:ident $state:ident $time:expr ) => {{
+        let event = KeyboardEvent::new(
+            Key::new(
+                stringify!($device),
+                key_code_from_str(stringify!($key)).unwrap(),
+            ),
+            i32_key_state_value_from_str(stringify!($state)),
+            mipoch($time),
+        );
+        $variable.receive(&event);
+    }};
+}
+
+macro_rules! check {
+    ( $variable:ident $method_1:ident $method_2:ident is $result:expr ) => {
+        assert!($variable.$method_1().$method_2() == $result);
+    };
+}
+
 #[test]
 fn key_down_event_within_interval() {
     let mut collector = Collector::new();
 
-    event!(collector receives: L1 D Down 0 => sequence 0, pending_cluster 1);
-    event!(collector receives: L1 F Down 4 => sequence 0, pending_cluster 2);
+    event!(collector receives: L1 D Down 0);
+    check!(collector sequence len is 0);
+    check!(collector pending_cluster len is 1);
+
+    event!(collector receives: L1 F Down 4);
+    check!(collector sequence len is 0);
+    check!(collector pending_cluster len is 2);
 }
 
 #[test]
 fn key_down_event_outside_interval() {
     let mut collector = Collector::new();
 
-    event!(collector receives: L1 D Down 0  => sequence 0, pending_cluster 1);
-    event!(collector receives: R1 J Down 22 => sequence 2, pending_cluster 0);
+    event!(collector receives: L1 D Down 0);
+    check!(collector sequence len is 0);
+    check!(collector pending_cluster len is 1);
+
+    event!(collector receives: R1 J Down 22);
+    check!(collector sequence len is 2);
+    check!(collector pending_cluster len is 0);
 }
 
 #[test]
 fn union_down_then_key_down_later() {
     let mut collector = Collector::new();
 
-    event!(collector receives: L1 D Down 0   => sequence 0, pending_cluster 1);
-    event!(collector receives: L1 F Down 10  => sequence 0, pending_cluster 2);
-    event!(collector receives: R1 J Down 100 => sequence 2, pending_cluster 0);
+    ev!(collector receives: L1 D Down 0   => sequence 0, pending_cluster 1);
+    ev!(collector receives: L1 F Down 10  => sequence 0, pending_cluster 2);
+    ev!(collector receives: R1 J Down 100 => sequence 2, pending_cluster 0);
 }
